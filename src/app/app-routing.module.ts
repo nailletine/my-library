be433e4acb13ed4cc80134e842c6b45448f6756f
nlajoie <nailletine.lajoie.prestataire@sfr.com>
2018-09-25 17:40:17 +0200

    Initialize router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+];
 
 
 @NgModule({
-  exports: [ RouterModule ]
+
+  imports: [ RouterModule.forRoot(routes) ],
+
+  exports: [ RouterModule],
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
+
+
